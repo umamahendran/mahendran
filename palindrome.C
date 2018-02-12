eee01609563bd2c725167cd1ff43c3382b6679d0
umamahendran <35994850+umamahendran@users.noreply.github.com>
2018-02-12 16:12:32 +0530

    Create palindrome.C

---
+++ palindrome.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int n,m,sum=0,p,i;
+scanf("%d",&n);
+p=n;
+for(i=0;n>0;i++)
+{
+m=n%10;
+sum=(sum*10)+m;
+n=n/10;
+}
+if(sum==p)
+printf("palindrome");
+else
+printf("not palindrome");
+getch();
+return 0;
+}
