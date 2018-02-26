f8abb2d5ac01aba9a204916d5a26d57c51d4d473
umamahendran <35994850+umamahendran@users.noreply.github.com>
2018-02-26 16:01:18 +0530

    Create nn.C

---
+++ nn.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int n,sum=0,i,a[100];
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+{
+sum=sum+a[i];
+}
+printf("median is %d",sum/n;
+}
+getch();
+return 0;
+}
