7202db72117bc7d63b3106d1ba994ba41c5032f0
umamahendran <35994850+umamahendran@users.noreply.github.com>
2018-02-26 16:20:53 +0530

    Create string.C

---
+++ string.C
@@ -0,0 +1,12 @@
+    #include<stdio.h>
+    int main()
+    {
+    char ch;
+    scanf("%c",&ch);
+    if(ch>='0' && ch<='9')
+    printf("YES");
+    else
+    printf("NO");
+    getch();
+    return 0;
+    }
