09661882eaf93b85434547d0465b741aa67d22b9
umamahendran <35994850+umamahendran@users.noreply.github.com>
2018-02-26 16:24:14 +0530

    Create ne.C

---
+++ ne.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n,max=0,i,a[10];
+max=a[0];
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+printf("\n %d %d",a[i],i);
+}
+getch();
+return 0;
+}
