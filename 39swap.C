20088743c9156dfabf9d87823fb65a81459e92de
umamahendran <35994850+umamahendran@users.noreply.github.com>
2018-03-02 13:44:33 +0530

    Create 39swap.C

---
+++ 39swap.C
@@ -0,0 +1,20 @@
+    #include<stdio.h>        
+    int main()
+    {
+    int n,a[10],i,max=-999;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+    scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+    if(a[i]>max)
+    {
+    max=a[i];
+    }
+    }
+    printf("%d",max);
+    GETCH();
+    return 0;
+    }
