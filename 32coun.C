93cf209d4f90254df33b733fd80f79e8139a100b
umamahendran <35994850+umamahendran@users.noreply.github.com>
2018-03-02 13:16:28 +0530

    Create 32coun.C

---
+++ 32coun.C
@@ -0,0 +1,20 @@
+include <stdio.h>
+#include <string.h>
+int main()
+{
+char s[100];
+int count=0,i;
+clrscr();
+printf("\n Enter the sentence:");
+scanf("%[^\n]s",s);
+for(i=0;s[i]!='\0';i++)
+{
+if(s[i]==' ')
+count++;    
+}
+printf("\n Number of words are: %d",count+1);
+{
+getch();
+return 0;
+}
+}
