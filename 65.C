ca1b5a9574360e3d927398562a860e6fa204a45c
umamahendran <35994850+umamahendran@users.noreply.github.com>
2018-03-16 16:03:04 +0530

    Create 65.C

---
+++ 65.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	char str[50];
+	int i;
+	printf("enter the string:");
+	scanf("%[^\n]s",str);
+	if(printf("\n%s",str))
+	{
+		
+	}
+  getch();
+  
+	return 0;
+}
