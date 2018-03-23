40889bba7a3aae267ffebf8633a1f63e2dd53655
umamahendran <35994850+umamahendran@users.noreply.github.com>
2018-03-23 15:48:05 +0530

    Create 73.C

---
+++ 73.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(void) 
+{
+	int n,l,r;
+	printf("\n enter the number:");
+	scanf("%d",&n);
+	printf("\n enter the left and right range:");
+	scanf("%d %d",&l,&r);
+	if(n>l && n<r)
+	{
+		printf("\n yes");
+	}
+	else
+	{
+		printf("\n no");
+	}
+	return 0;
+}
