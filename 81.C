c4c8a8376f7b0e1f0849467b56634f665e9f3776
umamahendran <35994850+umamahendran@users.noreply.github.com>
2018-03-26 15:45:00 +0530

    Create 81.C

---
+++ 81.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int n1,n2,i;
+	printf("\nenter the two numbers:");
+	for(i=1;i<=3;i++)
+	{
+	scanf("%d %d\n",&n1,&n2);
+	if(n1>n2)
+	{
+		printf("\n%d",n1-n2);
+	}
+	else
+	{
+		printf("\n%d",n2-n1);
+	}
+	}
+  getch();
+	return 0;
+}
